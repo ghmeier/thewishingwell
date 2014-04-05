2de15b06bc66d96a4934d32e242a0861820bf5a7
asr1 <alexandersrinehart@gmail.com>
2014-04-05 11:42:41 -0500

    Made about page. #CodeMonkeyReference

--- about.php
+++ about.php
@@ -8,11 +8,28 @@
 	
 	<body>
 		<a href="index.php" style="text-decoration:none;">This Takes you home.</a>
-		<!-- 
-		YO YO Yiggity yo, ALEX, the about text goes here, don't 
-		worry about formatting unless you want to take a 
-		stab at it :)
-		-->
+		<p><strong> What is The Wishing Well?<br></strong>
+		<em> The Wishing Well </em> is a digital version of the beloved classic.
+		In a real wishing well, you cast a hope and a coin, and wait for results. 
+		We've just modernized the concept.</p>
+		
+		<p><strong>Where does the money go?<br></strong>
+		Any money in the well will be collected at the end of the day and donated to the Make A Wish Foundation.</p>
+		
+		<p><strong>Wait, I can take money from the well?<br></strong>
+		Sure! As we said, our goal was to replicate a real wishing well. 
+		There's never anything stopping you from taking a coin from a real wishing well either, except that people are watching.
+		Here, your name will appear whenever you take or leave any money, but just to those who are watching. We don't record the information.
+		Just know that if you do take money,<strong> it will not be donated to the Make A Wish Foundation.</strong></p>
+		
+		<p><strong>So... what's the point?</strong><br>
+		The purpose of <em>The Wishing Well</em> is twofold.<br>
+		Firstly, The well allows you to share your heart's desire, focusing your dreams and optimism for the future,
+		all from the comfort of your own home. It serves the same purpose as a real wishing well, but for the modern era.<br>
+		Secondly, the well serves as a social experiment. There are dozens of theories about how people interact on the internet—
+		It's still being widely discussed. Many believe that anonymity brings out the worst in people Maybe that's true. But people don't often
+		take coins from real wishing wells. This project allows us to analyze how often people give verses take, how much people are donating,
+		and, in general, how people behave when something with established social norms is transformed to a medium where they are blurry at best.</p>
 	
 	</body>
 
